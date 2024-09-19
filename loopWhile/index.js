f2f60c3fbc4f091bea8ddb3bddef38f07389b989
vicky-rimbano <vivirimbano@gmail.com>
2024-09-19 08:21:52 -0300

    pasta loopWhile

---
+++ loopWhile/index.js
@@ -0,0 +1,30 @@
+while:
+let n = 1; 
+while (n <= 10) { 
+         loop finito
+         CTRL+C para parar o loop
+  console.log(n);
+  n++;
+}
+
+
+ let n = 1;
+ while(true){
+ let n2 = (Math.random() * 60).toFixed(2)
+ console.log(n2)
+ }
+
+
+ //while vs do while
+
+ let n = 1;
+ do {
+  console.log("----menu-----");
+        console.log("qual é o sue nome?");
+       console.log("-------------");
+        n = n + 1
+ } while(n < 10);
+
+
+//desafio 1
+//1) crie um loop que rode enquanto o usuario
