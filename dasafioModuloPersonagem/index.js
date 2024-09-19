8c2ed326bc1dbea2eb1fd76bb95b926e51359265
vicky-rimbano <vivirimbano@gmail.com>
2024-09-19 08:31:41 -0300

    pasta modulos: moduloPersonagem

---
+++ dasafioModuloPersonagem/index.js
@@ -0,0 +1,11 @@
+// crie um modulo ESM personagem.js
+
+// crie fuções para:
+// imprimir um objeto com (nome, poder, vida)
+// exporte a função (imprimirPersonagem)
+// importe o modulo dentro do sue arquivo
+// index.js
+
+import { imprimirPersonagem } from "./personagem.js";
+
+console.log(imprimirPersonagem("Navia", 17000, "ataquecritico"));
---
+++ dasafioModuloPersonagem/personagem.js
@@ -0,0 +1,9 @@
+export function imprimirPersonagem(nome , poder, vida){
+    return {
+      nome: nome, 
+      poder: poder, 
+      vida: vida
+    }
+  
+
+}
